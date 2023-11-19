dbd82fb3a90c3f6b1d74e321eb2d2a0fedd738f1
Emanuel Serveleon <123126936+emanuel-serveleon@users.noreply.github.com>
2023-11-19 18:04:25 -0500

    Add design and basic functions

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(a, b) { return a + b };
+function subtract(a, b) { return a - b };
+function multiply(a, b) { return a * b };
+function divide(a, b) { return a / b };
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+": return add(num1, num2)
+      break;
+    case "-": return subtract(num1, num2)
+      break;
+    case "×": return multiply(num1, num2)
+      break;
+    case "÷": return divide(num1, num2)
+      break;
+    default: return "Unknown operation"
+      break;
+  }
+}
