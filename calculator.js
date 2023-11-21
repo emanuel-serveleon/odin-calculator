70c55d8fa6b5e67bcd31064183708a467c520c57
Emanuel Serveleon <123126936+emanuel-serveleon@users.noreply.github.com>
2023-11-20 19:15:12 -0500

    Add functionality to log results in console
    Using temporarily the console to log the result and behavior of the
    calculator.
    Changed "add" function to add instead of concatenate

--- calculator.js
+++ calculator.js
@@ -1,14 +1,48 @@
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+let currentInput = "";
+
 const digitNodes = document.querySelectorAll(".digit");
 const displayNode = document.querySelector(".display");
 
 digitNodes.forEach ((digitNode) => digitNode.addEventListener("click", () => {
-  displayNode.textContent += digitNode.textContent;
+  //displayNode.textContent += digitNode.textContent;
+  currentInput += digitNode.textContent;
+  if (operator === "") {
+    number1 = currentInput;
+  } else {
+    number2 = currentInput;
+  }
+  console.log(`${number1} ${operator} ${number2}`);
 }))
+
+const operatorNodes = document.querySelectorAll(".operator")
+
+operatorNodes.forEach((operatorNode) => operatorNode.addEventListener("click", () => {
+  currentInput = "";
+  if (number2 !== "") {
+    number1 = operate(number1, number2, operator);
+  }
+  operator = operatorNode.textContent;
+  console.log(`${number1} ${operator}`);
+}))
+
+const equalsNode = document.querySelector(".equals");
+
+equalsNode.addEventListener("click", () => {
+  console.log(operate(number1, number2, operator));
+})
 
 const clearNode = document.querySelector(".clear");
 
 clearNode.addEventListener("click", () => {
   displayNode.textContent = "";
+  number1 = "";
+  number2 = "";
+  operator = "";
+  currentInput = "";
 })
 
 function add(a, b) { return a + b };
